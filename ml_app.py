818b796c9dbfe4f1f61c8dc4014641fb8822c2b6
indrapavan99 <139531117+indrapavan99@users.noreply.github.com>
2024-04-17 12:22:28 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -2,43 +2,42 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+
 from sklearn.tree import DecisionTreeClassifier
 
-#loading up the Classification model we created
-
-model = DecisionTreeClassifier(criterion='entropy', max_depth=10, min_samples_leaf=5,
-                       random_state=0)
+#Loading up the Classification model we created
+model = DecisionTreeClassifier(criterion='gini', max_depth=10, min_samples_leaf=5, random_state=0)
 model = joblib.load('finalized_model.joblib')
+#Caching the model for faster loading
+@st.cache
 
-@st.cache 
-
-def predict(Buying, Maint, Doors, Persons, Lug_boot, safety):
- if Safety == 'med':
-  Safety = 1
- elif Safety == 'high':
-  Safety = 2
- elif Safety == 'low':
-  Safety = 3
- df = pd.Dataframe([Buying, Maint, Doors, Persons, Lug_boot, safety])
- columns=(['buying','maint','doors','persons','lug_boot','safety'])
- prediction =model.predict([Buying, Maint, Doors, Persons, Lug_boot, safety])
- return prediction
+# Define the prediction function
+def predict(Buying, Maint,  Doors, Persons, Lug_boot, Safety):
+    if Safety == 'med':
+        safety = 1
+    elif Safety == 'high':
+        safety = 2
+    elif Safety == 'low':
+        safety = 3
+    df = pd.DataFrame([[Buying, Maint, Doors, Persons, Lug_boot, safety]], columns=['Buying', 'Maint',  'Doors','Persons', 'Lug_boot', 'Safety'])
+    prediction = model.predict([[Buying, Maint, Doors, Persons, Lug_boot, safety]])
+    return prediction
 
 st.title('Car Evaluation Classification')
-st.image("""https://media.zigcdn.com/media/model/2022/Jun/rear-3-4-right-1560931890_600x400.jpg""")
+st.image("""https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iRPmAdMaOVFg/v2/1800x1200.jpg""")
 st.header('Enter the Information of the Car:')
 st.text("vhigh = 1 high = 2 med = 3 low = 4")
 Buying = st.number_input('buying:', min_value=1, max_value=4, value=1)
-st.text("vhigh = 1 high = 2 med 3 low = 4")
+st.text("vhigh = 1 high = 2 med = 3 low = 4")
 Maint = st.number_input('maint:', min_value=1, max_value=4, value=1)
-st.text("2-Doors = 1 3-Doors 2 4-Doors = 3 5more = 4")
-Doors st.number_input('doors:', min_value=1, max_value=4, value=1)
-st.text("2-persons = 1 4-persons = 2 more = 3 ")
-Persons st.number_input('persons:', min_value=1, max_value=3, value=1)
-st.text("small 1 med 2 big = 3")
-Lug_boot st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
-Safety st.radio('safety:', ('med', 'high', 'low'))
+st.text("2-Doors = 1  3-Doors = 2 4-Doors = 3 5more = 4")
+Doors = st.number_input('doors:', min_value=1, max_value=4, value=1)
+st.text("2-persons = 1  4-persons = 2 more = 3 ")
+Persons = st.number_input('persons:', min_value=1, max_value=3, value=1)
+st.text("small = 1  med = 2 big = 3 ")
+Lug_boot = st.number_input('lug_boot:', min_value=1, max_value=3, value=1)
+Safety = st.radio('safety:', ('med', 'high', 'low'))
 
 if st.button('Submit_Car_Infos'):
-  cal_eval = predict(Buying, Maint, Doors, Persons, Lug_boot, safety)
-  st.success(f'The Evaluation of Car: {cal_eval[0]}')
+    cal_eval = predict(Buying, Maint, Doors, Persons, Lug_boot, Safety)
+    st.success(f'The Evaluation of Car : {cal_eval[0]}')
